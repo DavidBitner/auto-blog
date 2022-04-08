ee19408fdc0bddb512d09347048429c407f8ad3d
David <david-bitner@hotmail.com>
2022-04-08 15:32:56 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 import create_ripple from "./rippleEffect.js";
 
 const btns = document.getElementsByClassName("btn");
+const btnAdd = document.querySelector(`.btn`);
+const blogContainer = document.querySelector(`.blog`);
 
 for (const btn of btns) {
   btn.addEventListener("click", create_ripple);
